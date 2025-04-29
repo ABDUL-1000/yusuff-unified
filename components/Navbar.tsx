4a38e59666500dfd9527d73cba65bd6f1ddd9b99
ABDUL-1000 <abdullatifgiwa2019@gmail.com>
2025-04-29 23:26:18 +0100

    added page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ import MobileNavbar from './MobileNavbar'
 
 const Navbar = () => {
   return (
-    <nav className='sticky top-0 bg-bg/94  w-full h-16 z-50 backdrop-blur supports-[backdrop-filter]:bg/96'>
-      <div className='max-w-7xl lg:mx-[140px] px-4 bg-[#0C0C0C]'>
+    <nav className='sticky top-0 bg-bg/95  w-full h-16 z-50 backdrop-blur supports-[backdrop-filter]:bg-bg/96'>
+      <div className='max-w-7xl lg:mx-[140px] px-4 '>
         <div className='flex justify-between items-center h-16' >
             <div className='flex items-center'>
 
