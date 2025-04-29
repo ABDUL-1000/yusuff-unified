acdd061e15a2ef5a9df4796921a1d362736a4011
ABDUL-1000 <abdullatifgiwa2019@gmail.com>
2025-04-29 15:53:44 +0100

    third commit

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -13,7 +13,7 @@ const ModeToggle = () => {
     <Button
     variant='outline'
     size='icon'
-    onClick={()=> setTheme(theme === 'dark' ? 'light' : 'dark')}
+    onClick={()=> setTheme(theme === 'dark' ? 'dark' : 'dark')}
     >
     <MoonIcon className='h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:-scale-0'/>
     <SunIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:-scale-100'/>
